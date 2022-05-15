1cbe454e8142ecc6bddb22478e58d9d4b0d386f3
kotchaniphapk <kotchanipha.pk@gmail.com>
2022-05-15 20:57:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      borderRadius:{
+        xl:'1rem' ;
+        '2xl':'2rem'
+        '3xl':'3rem'
+
+      }
+    },
+  },
+  plugins: ['reponsive','hover'],
+}
